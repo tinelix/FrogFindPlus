0d9bb833c9f1b0fa0920230a41206a8370d7916a
Dmitry Tretyakov <76806170+tretdm@users.noreply.github.com>
2025-09-24 15:43:56 +0700

    Update about.php

--- about.php
+++ about.php
@@ -17,27 +17,27 @@
     <hr>
     <br>
     <center>
-    <h1><?php tr('about_what_is_t') ?></h1>
-    <small><?php tr('about_faq_short_description') ?></small>
+    <h1><?php tr('about_what_is_t'); ?></h1>
+    <small><?php tr('about_faq_short_description'); ?></small>
     </center>
     <br>
-    <h3><?php tr('about_who_made_q') ?></h3>
-    <?php tr('about_who_made_a') ?>
-    <h3><?php tr('about_how_does_q') ?></h3>
-    <?php tr('about_how_does_a') ?>
-    <h3><?php tr('about_what_machines_do_you_test_q') ?></h3>
-    <?php tr('about_what_machines_do_you_test_a') ?>
-    <h3><?php tr('about_how_can_i_get_in_touch_q') ?></h3>
-    <?php tr('about_what_machines_do_you_test_q') ?>
+    <h3><?php tr('about_who_made_q'); ?></h3>
+    <?php tr('about_who_made_a'); ?>
+    <h3><?php tr('about_how_does_q'); ?></h3>
+    <?php tr('about_how_does_a'); ?>
+    <h3><?php tr('about_what_machines_do_you_test_q'); ?></h3>
+    <?php tr('about_what_machines_do_you_test_a'); ?>
+    <h3><?php tr('about_how_can_i_get_in_touch_q'); ?></h3>
+    <?php tr('about_what_machines_do_you_test_q'); ?>
 	<center>
-	<h1><?php tr('about_what_is_frogfindplus_t') ?></h1>
+	<h1><?php tr('about_what_is_frogfindplus_t'); ?></h1>
     </center>
     <br>
-    <h3><?php tr('about_frogfindplus_whats_difference_q') ?></h3>
-    <?php tr('about_frogfindplus_whats_difference_a') ?>
-    <h3><?php tr('about_frogfindplus_who_translated_q') ?></h3>
-    <?php tr('about_frogfindplus_who_translated_a') ?>
-    <h3><?php tr('about_frogfindplus_does_have_source_q') ?></h3>
-    <?php tr('about_frogfindplus_does_have_source_a') ?>
+    <h3><?php tr('about_frogfindplus_whats_difference_q'); ?></h3>
+    <?php tr('about_frogfindplus_whats_difference_a'); ?>
+    <h3><?php tr('about_frogfindplus_who_translated_q'); ?></h3>
+    <?php tr('about_frogfindplus_who_translated_a'); ?>
+    <h3><?php tr('about_frogfindplus_does_have_source_q'); ?></h3>
+    <?php tr('about_frogfindplus_does_have_source_a'); ?>
 </body>
 </html>
