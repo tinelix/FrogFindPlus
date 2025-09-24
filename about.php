f0a3eb26dcb2e29267623a8d5133778d8ea17de3
Dmitry Tretyakov <76806170+tretdm@users.noreply.github.com>
2025-09-24 15:41:52 +0700

    Update about.php

--- about.php
+++ about.php
@@ -21,23 +21,23 @@
     <small><?= tr('about_faq_short_description') ?></small>
     </center>
     <br>
-    <h3><?= tr('about_who_made_q') ?></h3>
-    <?= tr('about_who_made_a') ?>
-    <h3><?= tr('about_how_does_q') ?></h3>
-    <?= tr('about_how_does_a') ?>
-    <h3><?= tr('about_what_machines_do_you_test_q') ?></h3>
-    <?= tr('about_what_machines_do_you_test_a') ?>
-    <h3><?= tr('about_how_can_i_get_in_touch_q') ?></h3>
-    <?= tr('about_what_machines_do_you_test_q') ?>
+    <h3><?php tr('about_who_made_q') ?></h3>
+    <?php tr('about_who_made_a') ?>
+    <h3><?php tr('about_how_does_q') ?></h3>
+    <?php tr('about_how_does_a') ?>
+    <h3><?php tr('about_what_machines_do_you_test_q') ?></h3>
+    <?php tr('about_what_machines_do_you_test_a') ?>
+    <h3><?php tr('about_how_can_i_get_in_touch_q') ?></h3>
+    <?php tr('about_what_machines_do_you_test_q') ?>
 	<center>
-	<h1><?= tr('about_what_is_frogfindplus_t') ?></h1>
+	<h1><?php tr('about_what_is_frogfindplus_t') ?></h1>
     </center>
     <br>
-    <h3><?= tr('about_frogfindplus_whats_difference_q') ?></h3>
-    <?= tr('about_frogfindplus_whats_difference_a') ?>
-    <h3><?= tr('about_frogfindplus_who_translated_q') ?></h3>
-    <?= tr('about_frogfindplus_who_translated_a') ?>
-    <h3><?= tr('about_frogfindplus_does_have_source_q') ?></h3>
-    <?= tr('about_frogfindplus_does_have_source_a') ?>
+    <h3><?php tr('about_frogfindplus_whats_difference_q') ?></h3>
+    <?php tr('about_frogfindplus_whats_difference_a') ?>
+    <h3><?php tr('about_frogfindplus_who_translated_q') ?></h3>
+    <?php tr('about_frogfindplus_who_translated_a') ?>
+    <h3><?php tr('about_frogfindplus_does_have_source_q') ?></h3>
+    <?php tr('about_frogfindplus_does_have_source_a') ?>
 </body>
 </html>
